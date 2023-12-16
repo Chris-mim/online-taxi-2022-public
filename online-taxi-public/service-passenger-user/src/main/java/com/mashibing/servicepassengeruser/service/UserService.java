6fbbe0875bd663e46e030143a97c5a21f801395e
zjj <544755024@qq.com>
2023-12-16 20:57:34 +0800

    添加mapper，根据手机号查询用户信息

--- online-taxi-public/service-passenger-user/src/main/java/com/mashibing/servicepassengeruser/service/UserService.java
+++ online-taxi-public/service-passenger-user/src/main/java/com/mashibing/servicepassengeruser/service/UserService.java
@@ -1,14 +1,29 @@
 package com.mashibing.servicepassengeruser.service;
 
 import com.mashibing.internalcommon.constant.ResponseResult;
+import com.mashibing.servicepassengeruser.dto.PassengerUser;
+import com.mashibing.servicepassengeruser.mapper.PassengerUserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 @Service
 public class UserService {
+
+    @Autowired
+    private PassengerUserMapper passengerUserMapper;
     public ResponseResult loginOrRegister(String passengerPhone) {
         System.out.println("user service被调用，手机号："+passengerPhone);
 
         // 根据手机号查询用户信息
+        Map<String, Object> map = new HashMap<>();
+        map.put("passenger_phone",passengerPhone);
+        List<PassengerUser> passengerUsers = passengerUserMapper.selectByMap(map);
+        System.out.println(passengerUsers.size()==0?"无记录":passengerUsers.get(0).getPassengerName());
 
         // 判断用户信息是否存在
 
