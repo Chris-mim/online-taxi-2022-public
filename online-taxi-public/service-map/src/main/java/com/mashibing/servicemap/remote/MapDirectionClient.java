1e1ec6be9476b0205451ddb83bf74c46349092a8
zjj <544755024@qq.com>
2023-12-21 23:46:40 +0800

    调用地图接口获取路径规划信息

---
+++ online-taxi-public/service-map/src/main/java/com/mashibing/servicemap/remote/MapDirectionClient.java
@@ -0,0 +1,87 @@
+package com.mashibing.servicemap.remote;
+
+import com.mashibing.internalcommon.constant.AmapConfigConstants;
+import com.mashibing.internalcommon.request.ForecastPriceDTO;
+import com.mashibing.internalcommon.response.DirectionResponse;
+import lombok.extern.slf4j.Slf4j;
+import net.sf.json.JSON;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@Slf4j
+public class MapDirectionClient {
+
+    @Value("${amap.key}")
+    private String amapKey;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public DirectionResponse direction(ForecastPriceDTO param) {
+        /**
+         *  https://restapi.amap.com/v3/direction/driving
+         *  ?origin=116.481028,39.989643
+         *  &destination=116.465302,40.004717
+         *  &extensions=base
+         *  &output=xml
+         *  &key=e86216c5e908fff38e9b6270a0cb8df0
+         */
+        StringBuilder urlBuilder = new StringBuilder();
+        urlBuilder.append(AmapConfigConstants.DIRECTION_URL);
+        urlBuilder.append("?");
+        urlBuilder.append("origin=" + param.getDepLongitude() + "," + param.getDepLatitude());
+        urlBuilder.append("&");
+        urlBuilder.append("destination=" + param.getDestLongitude() + "," + param.getDestLatitude());
+        urlBuilder.append("&");
+        urlBuilder.append("extensions=base");
+        urlBuilder.append("&");
+        urlBuilder.append("output=json");
+        urlBuilder.append("&");
+        urlBuilder.append("key=" + amapKey);
+        log.info("direction url :" + urlBuilder.toString());
+
+        ResponseEntity<String> directionEntity = restTemplate.getForEntity(urlBuilder.toString(), String.class);
+        String directionString = directionEntity.getBody();
+        log.info("高德地图：路径规划，返回信息：" + directionString);
+        // 解析接口
+        DirectionResponse directionResponse = parseDirectionEntity(directionString);
+        return directionResponse;
+    }
+
+    private DirectionResponse parseDirectionEntity(String directionString) {
+        DirectionResponse directionResponse = null;
+        try {
+            JSONObject result = JSONObject.fromObject(directionString);
+            if (result.has(AmapConfigConstants.STATUS)) {
+                int status = result.getInt(AmapConfigConstants.STATUS);
+                if (status == 1) {
+                    if (result.has((AmapConfigConstants.ROUTE))) {
+                        JSONObject routeObject = result.getJSONObject(AmapConfigConstants.ROUTE);
+                        JSONArray pathsArray = routeObject.getJSONArray(AmapConfigConstants.PATHS);
+                        JSONObject pathObject = pathsArray.getJSONObject(0);
+                        directionResponse = new DirectionResponse();
+                        if (pathObject.has(AmapConfigConstants.DISTANCE)) {
+                            int distance = pathObject.getInt(AmapConfigConstants.DISTANCE);
+                            directionResponse.setDistance(distance);
+                        }
+                        if (pathObject.has(AmapConfigConstants.DURATION)) {
+                            int duration = pathObject.getInt(AmapConfigConstants.DURATION);
+                            directionResponse.setDuration(duration);
+                        }
+
+                    }
+
+                }
+            }
+        } catch (Exception e) {
+
+        }
+        return directionResponse;
+    }
+}
--- online-taxi-public/service-map/src/main/java/com/mashibing/servicemap/service/DirectionService.java
+++ online-taxi-public/service-map/src/main/java/com/mashibing/servicemap/service/DirectionService.java
@@ -3,10 +3,15 @@ package com.mashibing.servicemap.service;
 import com.mashibing.internalcommon.dto.ResponseResult;
 import com.mashibing.internalcommon.request.ForecastPriceDTO;
 import com.mashibing.internalcommon.response.DirectionResponse;
+import com.mashibing.servicemap.remote.MapDirectionClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DirectionService {
+
+    @Autowired
+    private MapDirectionClient mapDirectionClient;
 
     /**
      * 根据起点经纬度和终点经纬获取距离（米）和时长（分钟）
@@ -14,11 +19,10 @@ public class DirectionService {
      * @return 距离（米）和时长（分钟）
      */
     public ResponseResult driving(ForecastPriceDTO forecastPriceDTO){
+        // 调用地图服务接口
+        DirectionResponse direction = mapDirectionClient.direction(forecastPriceDTO);
 
 
-        DirectionResponse directionResponse = new DirectionResponse();
-        directionResponse.setDistance(123);
-        directionResponse.setDuration(11);
-        return ResponseResult.success(directionResponse);
+        return ResponseResult.success(direction);
     }
 }
