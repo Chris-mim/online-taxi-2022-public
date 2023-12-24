ee7bb92e14621ffb16160a7a307a0e62694ffabb
zjj <544755024@qq.com>
2023-12-24 10:26:10 +0800

    service-driver-user 操作数据库骨架完成、日期处理

--- online-taxi-public/service-driver-user/src/main/java/com/mashibing/servicedriveruser/ServiceDriverUserApplication.java
+++ online-taxi-public/service-driver-user/src/main/java/com/mashibing/servicedriveruser/ServiceDriverUserApplication.java
@@ -1,9 +1,13 @@
 package com.mashibing.servicedriveruser;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@MapperScan("com.mashibing.servicedriveruser.mapper")
+@EnableDiscoveryClient
 public class ServiceDriverUserApplication {
 
     public static void main(String[] args) {
