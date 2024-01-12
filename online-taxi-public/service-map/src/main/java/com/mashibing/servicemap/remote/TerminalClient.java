335e129e88d8d36f2a45b300cd87b9ba46228aa5
zjj <544755024@qq.com>
2024-01-12 18:19:13 +0800

    添加注释

--- online-taxi-public/service-map/src/main/java/com/mashibing/servicemap/remote/TerminalClient.java
+++ online-taxi-public/service-map/src/main/java/com/mashibing/servicemap/remote/TerminalClient.java
@@ -3,6 +3,7 @@ package com.mashibing.servicemap.remote;
 import com.mashibing.internalcommon.constant.AmapConfigConstants;
 import com.mashibing.internalcommon.dto.ResponseResult;
 import com.mashibing.internalcommon.response.TerminalResponse;
+import lombok.extern.slf4j.Slf4j;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Slf4j
 public class TerminalClient {
 
     @Value("${amap.key}")
@@ -62,7 +64,12 @@ public class TerminalClient {
         response.setTid(tid);
         return ResponseResult.success(response);
     }
-
+    /**
+     * 周边搜索终端
+     * @param center 经纬度，string类型。格式为：纬度,经度
+     * @param radius int类型。单位：米，取值范围[1,5000]
+     * @return
+     */
     public ResponseResult<List<TerminalResponse>> aroundSearch(String center, Integer radius) {
 
         StringBuilder url = new StringBuilder();
@@ -76,10 +83,12 @@ public class TerminalClient {
         url.append("&");
         url.append("radius=" + radius);
 
+        log.info("周边搜索终端请求"+ url);
         ResponseEntity<String> entity = restTemplate.postForEntity(url.toString(),null, String.class);
         String body = entity.getBody();
         // 解析接口
         JSONObject responseJson = JSONObject.fromObject(body);
+        log.info("周边搜索终端响应 :"+ responseJson);
         JSONObject data = responseJson.getJSONObject("data");
         JSONArray results = data.getJSONArray("results");
         List<TerminalResponse> res = new ArrayList<>();
--- online-taxi-public/service-map/src/main/java/com/mashibing/servicemap/service/TerminalService.java
+++ online-taxi-public/service-map/src/main/java/com/mashibing/servicemap/service/TerminalService.java
@@ -16,7 +16,12 @@ public class TerminalService {
     public ResponseResult<TerminalResponse> add(String name, String desc) {
         return terminalClient.add(name,desc);
     }
-
+    /**
+     * 周边搜索终端
+     * @param center 经纬度，string类型。格式为：纬度,经度
+     * @param radius int类型。单位：米，取值范围[1,5000]
+     * @return
+     */
     public ResponseResult<List<TerminalResponse>> aroundSearch(String center, Integer radius) {
         return terminalClient.aroundSearch(center,radius);
     }
