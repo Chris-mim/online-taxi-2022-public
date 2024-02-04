876b0b5d1255619b742136f4da27174afc5ab51a
temp_zhangjj <jiajun.zhang01@hand-china.com>
2024-02-04 17:05:38 +0800

    发起收款接口设计&代码骨架

---
+++ online-taxi-public/api-driver/src/main/java/com/mashibing/apidriver/controller/PayController.java
@@ -0,0 +1,24 @@
+package com.mashibing.apidriver.controller;
+
+import com.mashibing.internalcommon.dto.ResponseResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/pay")
+public class PayController {
+
+    /**
+     * 司机发起收款
+     * @param orderId
+     * @param price
+     * @return
+     */
+    @PostMapping("/push-pay-info")
+    public ResponseResult pushPayInfo(@RequestParam String orderId , @RequestParam String price, @RequestParam Long passengerId){
+
+        return null;
+    }
+}
