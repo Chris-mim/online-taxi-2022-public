4d42878c968f32f9583d74a96e558205f94de1d6
zjj <544755024@qq.com>
2024-02-24 00:14:46 +0800

    测试支付宝能否访问通，我们的服务。

---
+++ online-taxi-public/test-alipay/src/main/java/com/mashibing/testalipay/TestAlipayApplication.java
@@ -0,0 +1,20 @@
+package com.mashibing.testalipay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TestAlipayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestAlipayApplication.class, args);
+    }
+
+    @GetMapping("/test")
+    public String test(){
+        return "外网穿透测试";
+    }
+}
---
+++ online-taxi-public/test-alipay/src/test/java/com/mashibing/testalipay/TestAlipayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mashibing.testalipay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAlipayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
