dce66f94d1389ef56551a0f3caa042e42f2a876a
zjj <544755024@qq.com>
2024-03-03 23:04:50 +0800

    验证框架，自定义响应值的包装。

--- online-taxi-public/internal-common/src/main/java/com/mashibing/internalcommon/constant/CommonStatusEnum.java
+++ online-taxi-public/internal-common/src/main/java/com/mashibing/internalcommon/constant/CommonStatusEnum.java
@@ -63,6 +63,13 @@ public enum CommonStatusEnum {
     CITY_SERVICE_NOT_SERVICE(1602,"当前城市不提供叫车服务"),
 
     ORDER_CANCEL_ERROR(1603, "订单取消失败"),
+
+    /**
+     * 统一验证提示 1700-1799
+     */
+    VALIDATION_EXCEPTION(1700,"统一验证框架的错误提示"),
+
+
     /**
      * 成功
      */
@@ -73,6 +80,7 @@ public enum CommonStatusEnum {
     FAIL(0,"fail")
 
     ;
+
     @Getter
     private int code;
     @Getter
